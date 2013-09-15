38e4af5c89dcbbab227f3d3271689a13665f34c4
gark87 <my_another@mail.ru>
2013-09-15 16:08:35 +0400

    nullGuards extract

--- yajom-macros/src/org/gark87/yajom/macros/nullSafe.scala
+++ yajom-macros/src/org/gark87/yajom/macros/nullSafe.scala
@@ -26,78 +26,9 @@ object nullSafe {
 
     def createVal(name: TermName, value: Expr[_], tpe: Type) = ValDef(Modifiers(), name, TypeTree(tpe), value.tree)
 
-
-    def withNullGuards(expr: c.Expr[(T) => Unit]): c.Expr[(T) => Unit] = {
-      var prefix: List[c.universe.Tree] = List()
-
-      def addNullGuards(tree: Tree): Tree = {
-        tree match {
-          case Apply(Select(qualifier, name), List()) =>
-            val getterName = name.decoded
-            val getter: MethodSymbol = qualifier.tpe.member(name).asMethod
-            val correctParams = getter.paramss match {
-              case List(List()) => true
-              case _ => false
-            }
-            val correctName = getterName.startsWith("get") || getterName.startsWith("is")
-            val setterName = getterName.replaceFirst("^(is|get)", "set")
-            val returnType: c.Type = getter.returnType
-            val setter = qualifier.tpe.members.find((x: Symbol) => {
-              if (!x.isMethod)
-                false
-              else {
-                val method: MethodSymbol = x.asMethod
-                val correctSetterParams = method.paramss match {
-                  case List(List(termSymbol)) => termSymbol.asTerm.typeSignature == returnType
-                  case _ => false
-                }
-                method.isMethod && correctSetterParams && method.isPublic && setterName == method.name.decoded
-              }
-            })
-            val newQ = addNullGuards(qualifier)
-            if (!correctName || !correctParams) {
-              Select(newQ, name)
-            } else if (setter.isEmpty) {
-              reporter.error("Cannot find setter")
-            } else {
-              val synthetic = (0l + scala.reflect.internal.Flags.SYNTHETIC).asInstanceOf[c.universe.FlagSet]
-              val resultValue = newTermName(c.fresh("resultValue$"))
-              val e = c.Expr[Any](tree)
-              val o = creator.createDefaultObject(c) (returnType, objectFactoryType)
-
-              prefix = prefix :+ ValDef(Modifiers(synthetic), resultValue, TypeTree(),
-                reify {
-                  val tmp = e.splice
-                  if (tmp == null)
-                    o.splice
-                  else
-                    tmp
-                }.tree
-              )
-              Ident(resultValue)
-            }
-          case Apply(fun, args) =>
-            Apply(addNullGuards(fun), args)
-          case Block(stats, epr) =>
-            Block(stats.map((s: Tree) => {
-              addNullGuards(s)
-            }), epr)
-          case ValDef(mods, name, tpt, rhs) =>
-            ValDef(mods, name, tpt, addNullGuards(rhs))
-          case a => reporter.error("Too complex expression `" + a + "` for YAJOM:\n1. Quick Fix: extract val without YAJOM\n2. mail gark87 <my_another@mail.ru>")
-        }
-      }
-      val guards: c.universe.Tree = addNullGuards(expr.tree)
-      guards match {
-        case Block(a, b) => val d: List[c.universe.Tree] = prefix ::: a
-          c.Expr[T => Unit](Block(d, c.resetAllAttrs(b)))
-        case _ => reporter.error("no need of `nullSafe'")
-      }
-    }
-
     if (fromType == toType) {
       val castedFrom: c.Expr[T] = from.asInstanceOf[c.Expr[T]]
-      val guards = withNullGuards(setter)
+      val guards = withNullGuards(c)(setter, creator, objectFactoryType)
       c.Expr[Unit](reify {
         guards.splice(castedFrom.splice)
       }.tree)
@@ -123,5 +54,77 @@ object nullSafe {
     }
   }
 
+
+  def withNullGuards[T: c.WeakTypeTag](c: reflect.macros.Context)(expr: c.Expr[(T) => Unit], creator: ObjectCreator, objectFactoryType: c.Type): c.Expr[(T) => Unit] = {
+    import c.universe._
+
+    val reporter = creator.reporter
+    var prefix: List[c.universe.Tree] = List()
+
+    def addNullGuards(tree: Tree): Tree = {
+      tree match {
+        case Apply(Select(qualifier, name), List()) => {
+          val getterName = name.decoded
+          val getter: MethodSymbol = qualifier.tpe.member(name).asMethod
+          val correctParams = getter.paramss match {
+            case List(List()) => true
+            case _ => false
+          }
+          val correctName = getterName.startsWith("get") || getterName.startsWith("is")
+          val setterName = getterName.replaceFirst("^(is|get)", "set")
+          val returnType: c.Type = getter.returnType
+          val setter = qualifier.tpe.members.find((x: Symbol) => {
+            if (!x.isMethod)
+              false
+            else {
+              val method: MethodSymbol = x.asMethod
+              val correctSetterParams = method.paramss match {
+                case List(List(termSymbol)) => termSymbol.asTerm.typeSignature == returnType
+                case _ => false
+              }
+              method.isMethod && correctSetterParams && method.isPublic && setterName == method.name.decoded
+            }
+          })
+          val newQ = addNullGuards(qualifier)
+          if (!correctName || !correctParams) {
+            Select(newQ, name)
+          } else if (setter.isEmpty) {
+            reporter.error("Cannot find setter")
+          } else {
+            val synthetic = (0l + scala.reflect.internal.Flags.SYNTHETIC).asInstanceOf[c.universe.FlagSet]
+            val resultValue = newTermName(c.fresh("resultValue$"))
+            val e = c.Expr[Any](tree)
+            val o = creator.createDefaultObject(c)(returnType, objectFactoryType)
+
+            prefix = prefix :+ ValDef(Modifiers(synthetic), resultValue, TypeTree(),
+              reify {
+                val tmp = e.splice
+                if (tmp == null)
+                  o.splice
+                else
+                  tmp
+              }.tree
+            )
+            Ident(resultValue)
+          }
+        }
+        case Apply(fun, args) =>
+          Apply(addNullGuards(fun), args)
+        case Block(stats, epr) =>
+          Block(stats.map((s: Tree) => {
+            addNullGuards(s)
+          }), epr)
+        case ValDef(mods, name, tpt, rhs) =>
+          ValDef(mods, name, tpt, addNullGuards(rhs))
+        case a => reporter.error("Too complex expression `" + a + "` for YAJOM:\n1. Quick Fix: extract val without YAJOM\n2. mail gark87 <my_another@mail.ru>")
+      }
+    }
+    val guards: c.universe.Tree = addNullGuards(expr.tree)
+    guards match {
+      case Block(a, b) => val d: List[c.universe.Tree] = prefix ::: a
+        c.Expr[T => Unit](Block(d, c.resetAllAttrs(b)))
+      case _ => reporter.error("no need of `nullSafe'")
+    }
+  }
 }
 
