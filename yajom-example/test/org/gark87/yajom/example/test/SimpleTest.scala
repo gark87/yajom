cec02482a1828ab62bca200ce421e99c76790800
gark87 <my_another@mail.ru>
2013-10-04 01:02:00 +0400

    map comes back

--- yajom-example/test/org/gark87/yajom/example/test/SimpleTest.scala
+++ yajom-example/test/org/gark87/yajom/example/test/SimpleTest.scala
@@ -1,8 +1,8 @@
 package org.gark87.yajom.example.test
 
 import org.gark87.yajom.base.BaseMapper
-import org.gark87.yajom.example.to.Person
-import org.gark87.yajom.example.from.Employee
+import org.gark87.yajom.example.to.{Gender, Person}
+import org.gark87.yajom.example.from.{Details, Employee}
 import org.gark87.yajom.macros.Facade._
 import org.junit.Assert._
 import java.util.Date
@@ -19,19 +19,39 @@ class SimpleMapper extends BaseMapper {
   }
 }
 
+class MapSimpleMapper extends BaseMapper {
+  def map(from: Person, to: Employee) {
+    yajomMap(to.setDetails)(from.getBirthDate)
+  }
+
+  def map(from: Date, to: Details) {
+    yajom(to.setBirth)(from)
+  }
+}
+
 class SimpleTest extends MapperTest {
   private val mapper = new SimpleMapper
+  private val mapMapper = new MapSimpleMapper
+
 
   @Test def testSimple() {
     val employee = mapper.toEmployee(person)
-    assertNotNull(employee)
-    assertNotNull(employee.getDetails)
-    assertNull(employee.getDetails.getBirth)
+    val to = new Employee
+    mapMapper.map(person, to)
+    List(to, employee).foreach(employee => {
+      assertNotNull(employee)
+      assertNotNull(employee.getDetails)
+      assertNull(employee.getDetails.getBirth)
+    })
   }
 
   @Test def testFilledDetails() {
     person.setBirthDate(new Date(0))
-    val employee: Employee = mapper.toEmployee(person)
-    assertEquals(new Date(0), employee.getDetails.getBirth)
+    val employee = mapper.toEmployee(person)
+    val to = new Employee
+    mapMapper.map(person, to)
+    List(to, employee).foreach(employee => {
+      assertEquals(new Date(0), employee.getDetails.getBirth)
+    })
   }
 }
--- yajom-macros/src/org/gark87/yajom/macros/Facade.scala
+++ yajom-macros/src/org/gark87/yajom/macros/Facade.scala
@@ -2,9 +2,12 @@ package org.gark87.yajom.macros
 
 import org.gark87.yajom.base.BaseMapper
 import language.experimental.macros
+import scala.reflect.macros.Context
 
 
 object Facade {
+  def yajomMap[T, F, M <: BaseMapper[_]](setter: (T) => Unit)(from: F)(implicit m: M): Unit = macro yajomMapImpl[T, F, M]
+
   def yajom[T, M <: BaseMapper[_]](setter: (T) => Unit)(from: T)(implicit m: M): Unit = macro yajomImpl[T, M]
 
   def yajomOption[T, M <: BaseMapper[_]](setter: (T) => Unit)(from: Option[T])(implicit m: M): Unit = macro optionImpl[T, M]
@@ -16,13 +19,52 @@ object Facade {
     import c.universe._
 
     val y = new YajomContext(c)
-    val objectFactoryType: y.c.Type = m.actualType.asInstanceOf[TypeRef].args.head.asInstanceOf[y.c.Type]
+    val objectFactoryType = m.actualType.asInstanceOf[TypeRef].args.head.asInstanceOf[y.c.Type]
     val guards = y.c.Expr[(T) => Unit](y.createOnNull.process(y)(setter.asInstanceOf[y.c.Expr[(T) => Unit]], objectFactoryType))
     c.Expr[Unit](reify {
       import scala.reflect.ClassTag
       guards.splice(from.splice)
     }.tree)
+  }
 
+  def yajomMapImpl[T: c.WeakTypeTag, F: c.WeakTypeTag, M <: BaseMapper[_]](c: reflect.macros.Context)(setter: c.Expr[(T) => Unit])(from: c.Expr[F])(m: c.Expr[M])
+  : c.Expr[Unit] = {
+    import c.universe._
+
+    val thisRef = m match {
+      case Expr(Select(a, _)) => a
+      case _ => c.abort(c.enclosingPosition, "OOPS")
+    }
+    val fromType = from.actualType
+    val toType = setter.actualType match {
+      case TypeRef(_, _, q) => q.head
+      case _ => c.abort(c.enclosingPosition, "OOPS")
+    }
+    val y = new YajomContext(c)
+    val objectFactoryType = m.actualType.asInstanceOf[TypeRef].args.head.asInstanceOf[y.c.Type]
+
+    val freshName = newTermName(c.fresh("fromValue$"))
+    val nonTrivialName = newTermName(c.fresh("nonTrivial$"))
+    val freshIdent = c.Expr[Any](Ident(freshName))
+    val mapCall = c.Expr[Any](Apply(Select(thisRef, newTermName("map")), List(Ident(freshName), Ident(nonTrivialName))))
+    def createVal(name: TermName, value: Expr[_], tpe: Type) = ValDef(Modifiers(), name, TypeTree(tpe), value.tree)
+    val fromValueCalc = createVal(freshName, reify {
+      from.splice
+    }, fromType)
+    val defaultObject = y.creator.createDefaultObject(y)(toType.asInstanceOf[y.c.Type], objectFactoryType)
+    val nonTrivialCall = createVal(nonTrivialName, c.Expr[Any](defaultObject.asInstanceOf[c.Tree]), toType)
+    val nonTrivialIdent = c.Expr[T](Ident(nonTrivialName))
+
+    val mapCall2 = reify {
+      import scala.reflect.ClassTag
+      if (nonTrivialIdent.splice != null) {
+        mapCall.splice
+        setter.splice(nonTrivialIdent.splice)
+      }
+    }.tree
+    c.Expr[Unit](
+      Block(fromValueCalc, nonTrivialCall, mapCall2)
+    )
   }
 
   def optionImpl[T: c.WeakTypeTag, M <: BaseMapper[_]](c: reflect.macros.Context)(setter: c.Expr[(T) => Unit])(from: c.Expr[Option[T]])(m: c.Expr[M])
